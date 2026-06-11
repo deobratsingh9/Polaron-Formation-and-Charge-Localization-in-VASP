643ea8779cb9bb45c4bdad98400927117d8946bf
Deobrat QMatX <137123400+deobratsingh9@users.noreply.github.com>
2026-06-11 18:11:58 +0200

    Add files via upload

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 #SBATCH -N 2 --exclusive
 #SBATCH --ntasks-per-node=32
 #SBATCH -t 004:00:00
-#SBATCH -A naiss2025-22-1789
+#SBATCH -A xxx
 
 module load VASP/5.4.4.16052018-nsc1-intel-2018a-eb
 mpprun vasp_std
